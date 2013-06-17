21f2dc5336fee4b720c36e21f76ce9440b921e83
QuangTVSE61078 <QuangTVSE61078@fpt.edu.vn>
2013-06-17 12:21:58 +0000

    Update Customer and Import/Export

---
+++ CocBook/DataAccessLayer/DTO/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayer.DTO
+{
+    class Customer
+    {
+        public string CustomerID { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string TaxNo { get; set; }
+    }
+}
