355e61519291d13bb5408ff6ab3c8d253a69855e
QuangTVSE61078@fpt.edu.vn <QuangTVSE61078@fpt.edu.vn@402803e5-b270-9f27-0cce-e9f6967a1f5f>
2013-06-17 15:20:01 +0000

    Update IE and Customer

---
+++ trunk/CocBook/DataAccessLayer.cs/DTO/IE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayer.cs.DTO
+{
+    class IE
+    {
+        public int CheckNo { get; set; }
+        public DateTime Date { get; set; }
+        public string Type { get; set; }
+        public string IE { get; set; }
+        public string CustomerID { get; set; }
+    }
+}
