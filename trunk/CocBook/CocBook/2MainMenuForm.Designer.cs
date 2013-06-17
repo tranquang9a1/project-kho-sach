74c11a37ac51628a0f6389fd08f65be85e3a5719
GiangHHTSE60894@fpt.edu.vn <GiangHHTSE60894@fpt.edu.vn@402803e5-b270-9f27-0cce-e9f6967a1f5f>
2013-06-17 07:03:29 +0000

    Update Database and Prototype

---
+++ trunk/CocBook/CocBook/2MainMenuForm.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace CocBook
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnRetailManage = new System.Windows.Forms.Button();
+            this.btnConsignManage = new System.Windows.Forms.Button();
+            this.txtBookManage = new System.Windows.Forms.Button();
+            this.txtViewMainStore = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRetailManage);
+            this.groupBox1.Controls.Add(this.btnConsignManage);
+            this.groupBox1.Controls.Add(this.txtBookManage);
+            this.groupBox1.Controls.Add(this.txtViewMainStore);
+            this.groupBox1.Location = new System.Drawing.Point(247, 29);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 218);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Công việc: ";
+            // 
+            // btnRetailManage
+            // 
+            this.btnRetailManage.Location = new System.Drawing.Point(25, 146);
+            this.btnRetailManage.Name = "btnRetailManage";
+            this.btnRetailManage.Size = new System.Drawing.Size(178, 23);
+            this.btnRetailManage.TabIndex = 3;
+            this.btnRetailManage.Text = "Quản lí bán lẻ";
+            this.btnRetailManage.UseVisualStyleBackColor = true;
+            // 
+            // btnConsignManage
+            // 
+            this.btnConsignManage.Location = new System.Drawing.Point(25, 102);
+            this.btnConsignManage.Name = "btnConsignManage";
+            this.btnConsignManage.Size = new System.Drawing.Size(178, 23);
+            this.btnConsignManage.TabIndex = 2;
+            this.btnConsignManage.Text = "Quản lí ký gửi";
+            this.btnConsignManage.UseVisualStyleBackColor = true;
+            // 
+            // txtBookManage
+            // 
+            this.txtBookManage.Location = new System.Drawing.Point(25, 29);
+            this.txtBookManage.Name = "txtBookManage";
+            this.txtBookManage.Size = new System.Drawing.Size(178, 23);
+            this.txtBookManage.TabIndex = 1;
+            this.txtBookManage.Text = "Quản lí sách";
+            this.txtBookManage.UseVisualStyleBackColor = true;
+            // 
+            // txtViewMainStore
+            // 
+            this.txtViewMainStore.Location = new System.Drawing.Point(25, 63);
+            this.txtViewMainStore.Name = "txtViewMainStore";
+            this.txtViewMainStore.Size = new System.Drawing.Size(178, 23);
+            this.txtViewMainStore.TabIndex = 0;
+            this.txtViewMainStore.Text = "Xem kho";
+            this.txtViewMainStore.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = global::CocBook.Properties.Resources.Logo;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(29, 92);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 272);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form2";
+            this.Text = "Main Menu";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button txtBookManage;
+        private System.Windows.Forms.Button txtViewMainStore;
+        private System.Windows.Forms.Button btnRetailManage;
+        private System.Windows.Forms.Button btnConsignManage;
+    }
+}
---
+++ trunk/CocBook/CocBook/3_1BookDetail.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace CocBook
+{
+    partial class _3_1BookDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtISBN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBookName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPulisher = new System.Windows.Forms.TextBox();
+            this.txtUnit = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtUnit);
+            this.groupBox1.Controls.Add(this.txtPulisher);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtBookName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtISBN);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(101, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(295, 175);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thông tin chi tiết";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(121, 209);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Lưu";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(295, 210);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Hủy";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(17, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 14);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã ISBN:";
+            // 
+            // txtISBN
+            // 
+            this.txtISBN.Location = new System.Drawing.Point(105, 27);
+            this.txtISBN.Name = "txtISBN";
+            this.txtISBN.Size = new System.Drawing.Size(164, 20);
+            this.txtISBN.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 14);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Tên sách:";
+            // 
+            // txtBookName
+            // 
+            this.txtBookName.Location = new System.Drawing.Point(105, 62);
+            this.txtBookName.Name = "txtBookName";
+            this.txtBookName.Size = new System.Drawing.Size(164, 20);
+            this.txtBookName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 14);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Nhà xuất bản:";
+            // 
+            // txtPulisher
+            // 
+            this.txtPulisher.Location = new System.Drawing.Point(105, 96);
+            this.txtPulisher.Name = "txtPulisher";
+            this.txtPulisher.Size = new System.Drawing.Size(164, 20);
+            this.txtPulisher.TabIndex = 5;
+            // 
+            // txtUnit
+            // 
+            this.txtUnit.Location = new System.Drawing.Point(105, 127);
+            this.txtUnit.Name = "txtUnit";
+            this.txtUnit.Size = new System.Drawing.Size(164, 20);
+            this.txtUnit.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 14);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Đơn vị tính:";
+            // 
+            // _3_1BookDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 261);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "_3_1BookDetail";
+            this.Text = "Chi tiết";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtUnit;
+        private System.Windows.Forms.TextBox txtPulisher;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtISBN;
+        private System.Windows.Forms.Label label1;
+    }
+}
