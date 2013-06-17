775e8bf401de988d19733ce309e738a37f76b781
GiangHHTSE60894@fpt.edu.vn <GiangHHTSE60894@fpt.edu.vn@402803e5-b270-9f27-0cce-e9f6967a1f5f>
2013-06-17 11:25:36 +0000

    DTO and DLA of Publisher

---
+++ trunk/CocBook/DataAccessLayer.cs/DTO/Publisher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayer.cs.DTO
+{
+    class Publisher
+    {
+        public int PublisherID { get; set; }
+        public string PublisherName { get; set; }
+    }
+}
