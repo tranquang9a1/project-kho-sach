8257f19cacd7f70a4e4056585c4b20873e1bf09c
ngocthse61095 <NgocTHSE61095@fpt.edu.vn>
2013-06-19 07:47:51 +0000

    Ngoc's Part has been done! :)

--- CocBook/CocBook/BookManange.Designer.cs
+++ CocBook/CocBook/BookManange.Designer.cs
@@ -30,9 +30,9 @@
         {
             this.BookGridView = new System.Windows.Forms.DataGridView();
             this.ISBN = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Unit = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.BookManange = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Publisher = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Unit = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnEdit = new System.Windows.Forms.Button();
@@ -42,85 +42,97 @@
             // 
             // BookGridView
             // 
-            this.BookGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.BookGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.BookGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
             this.BookGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ISBN,
-            this.Name,
-            this.Unit,
+            this.BookManange,
             this.Publisher,
+            this.Unit,
             this.Price});
-            this.BookGridView.Location = new System.Drawing.Point(60, 85);
+            this.BookGridView.Location = new System.Drawing.Point(60, 79);
             this.BookGridView.Name = "BookGridView";
-            this.BookGridView.Size = new System.Drawing.Size(662, 422);
+            this.BookGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.BookGridView.Size = new System.Drawing.Size(662, 392);
             this.BookGridView.TabIndex = 0;
+            this.BookGridView.Click += new System.EventHandler(this.BookGridView_Click);
             // 
             // ISBN
             // 
+            this.ISBN.DataPropertyName = "ISBNBook";
             this.ISBN.HeaderText = "ISBN";
             this.ISBN.Name = "ISBN";
             // 
-            // Name
+            // BookManange
             // 
-            this.Name.HeaderText = "Tên sách";
-            this.Name.Name = "Name";
-            this.Name.Width = 250;
+            this.BookManange.DataPropertyName = "BookName";
+            this.BookManange.HeaderText = "Tên sách";
+            this.BookManange.Name = "BookManange";
+            this.BookManange.Width = 250;
+            // 
+            // Publisher
+            // 
+            this.Publisher.DataPropertyName = "PublisherName";
+            this.Publisher.HeaderText = "Nhà xuất bản";
+            this.Publisher.Name = "Publisher";
             // 
             // Unit
             // 
+            this.Unit.DataPropertyName = "Unit";
             this.Unit.HeaderText = "Đơn vị tính";
             this.Unit.Name = "Unit";
             this.Unit.Width = 70;
             // 
-            // Publisher
-            // 
-            this.Publisher.HeaderText = "Nhà xuất bản";
-            this.Publisher.Name = "Publisher";
-            // 
             // Price
             // 
+            this.Price.DataPropertyName = "Price";
             this.Price.HeaderText = "Giá bìa";
             this.Price.Name = "Price";
             // 
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(185, 36);
+            this.btnAdd.Location = new System.Drawing.Point(185, 33);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Size = new System.Drawing.Size(75, 21);
             this.btnAdd.TabIndex = 1;
             this.btnAdd.Text = "Thêm";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // btnEdit
             // 
             this.btnEdit.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnEdit.Location = new System.Drawing.Point(334, 36);
+            this.btnEdit.Location = new System.Drawing.Point(334, 33);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.Size = new System.Drawing.Size(75, 21);
             this.btnEdit.TabIndex = 2;
             this.btnEdit.Text = "Sửa";
             this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // btnDelete
             // 
             this.btnDelete.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnDelete.Location = new System.Drawing.Point(481, 36);
+            this.btnDelete.Location = new System.Drawing.Point(481, 33);
             this.btnDelete.Name = "btnDelete";
-            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.Size = new System.Drawing.Size(75, 21);
             this.btnDelete.TabIndex = 3;
             this.btnDelete.Text = "Xóa";
             this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // BookManage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(781, 557);
+            this.ClientSize = new System.Drawing.Size(781, 517);
             this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnEdit);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.BookGridView);
-            this.Name.Name = "BookManange";
             this.Text = "Quản lí sách";
             ((System.ComponentModel.ISupportInitialize)(this.BookGridView)).EndInit();
             this.ResumeLayout(false);
@@ -133,10 +145,11 @@
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnEdit;
         private System.Windows.Forms.Button btnDelete;
-        private System.Windows.Forms.DataGridViewTextBoxColumn ISBN;
         private System.Windows.Forms.DataGridViewTextBoxColumn Name;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Unit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ISBN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn BookManange;
         private System.Windows.Forms.DataGridViewTextBoxColumn Publisher;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Unit;
         private System.Windows.Forms.DataGridViewTextBoxColumn Price;
     }
 }
