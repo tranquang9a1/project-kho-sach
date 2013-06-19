f2679362a937b3be47c1da224ae310669a7af745
QuangTVSE61078@fpt.edu.vn <QuangTVSE61078@fpt.edu.vn@402803e5-b270-9f27-0cce-e9f6967a1f5f>
2013-06-19 13:02:08 +0000

    Customer Detail and ChooseBook

--- trunk/CocBook/CocBook/ChooseBook.cs
+++ trunk/CocBook/CocBook/ChooseBook.cs
@@ -6,14 +6,61 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using DataAccessLayer.cs.DTO;
+using DataAccessLayer.cs.DAL;
 
 namespace CocBook
 {
     public partial class ChooseBook : Form
     {
+        Book book = new Book();
         public ChooseBook()
         {
             InitializeComponent();
+            LoadAllData();
+        }
+        public void LoadAllData()
+        {
+            BookDAL BookDAL = new BookDAL();
+            List<Book> list = BookDAL.GetAllBook();
+
+            BookGridView1.DataSource = list;
+
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            BookDetail bookDetail = new BookDetail();
+            bookDetail.Add = true;
+            bookDetail.NotifyEvent += new NotifyLoadData(BookDetail_NotifyEvent);
+            bookDetail.Show();
+        }
+        void BookDetail_NotifyEvent()
+        {
+            LoadAllData();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            BookDetail bookDetail = new BookDetail();
+            bookDetail.book = book;
+            bookDetail.Add = false;
+            bookDetail.LoadData();
+            bookDetail.NotifyEvent += new NotifyLoadData(BookDetail_NotifyEvent);
+            bookDetail.Show();
+        }
+        private void BookGridView_Click(object sender, EventArgs e)
+        {
+
+            book.ISBNBook = BookGridView1.SelectedRows[0].Cells[0].Value.ToString();
+            book.BookName = BookGridView1.SelectedRows[0].Cells[1].Value.ToString();
+            book.PublisherName = BookGridView1.SelectedRows[0].Cells[2].Value.ToString();
+            book.Unit = BookGridView1.SelectedRows[0].Cells[3].Value.ToString();
+            book.Price = (int)BookGridView1.SelectedRows[0].Cells[4].Value;
+        }
+
+        private void btnChoose_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
 }
--- trunk/CocBook/CocBook/ImExManage.Designer.cs
+++ trunk/CocBook/CocBook/ImExManage.Designer.cs
@@ -33,8 +33,8 @@
             this.btnEdit = new System.Windows.Forms.Button();
             this.btnView = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.rdImport = new System.Windows.Forms.RadioButton();
             this.rdExport = new System.Windows.Forms.RadioButton();
+            this.rdImport = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -43,7 +43,7 @@
             // 
             this.btnAdd.Location = new System.Drawing.Point(3, 5);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.Size = new System.Drawing.Size(75, 21);
             this.btnAdd.TabIndex = 0;
             this.btnAdd.Text = "Thêm";
             this.btnAdd.UseVisualStyleBackColor = true;
@@ -52,25 +52,25 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(47, 121);
+            this.dataGridView1.Location = new System.Drawing.Point(47, 112);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(569, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(569, 139);
             this.dataGridView1.TabIndex = 1;
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(191, 34);
+            this.btnEdit.Location = new System.Drawing.Point(194, 37);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.Size = new System.Drawing.Size(75, 21);
             this.btnEdit.TabIndex = 2;
             this.btnEdit.Text = "Sửa";
             this.btnEdit.UseVisualStyleBackColor = true;
             // 
             // btnView
             // 
-            this.btnView.Location = new System.Drawing.Point(88, 34);
+            this.btnView.Location = new System.Drawing.Point(87, 38);
             this.btnView.Name = "btnView";
-            this.btnView.Size = new System.Drawing.Size(75, 23);
+            this.btnView.Size = new System.Drawing.Size(75, 21);
             this.btnView.TabIndex = 3;
             this.btnView.Text = "Xem";
             this.btnView.UseVisualStyleBackColor = true;
@@ -80,38 +80,38 @@
             this.panel1.Controls.Add(this.rdExport);
             this.panel1.Controls.Add(this.rdImport);
             this.panel1.Controls.Add(this.btnAdd);
-            this.panel1.Location = new System.Drawing.Point(313, 34);
+            this.panel1.Location = new System.Drawing.Point(313, 32);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(211, 35);
+            this.panel1.Size = new System.Drawing.Size(211, 32);
             this.panel1.TabIndex = 4;
-            // 
-            // rdImport
-            // 
-            this.rdImport.AutoSize = true;
-            this.rdImport.Location = new System.Drawing.Point(84, 7);
-            this.rdImport.Name = "rdImport";
-            this.rdImport.Size = new System.Drawing.Size(47, 18);
-            this.rdImport.TabIndex = 1;
-            this.rdImport.TabStop = true;
-            this.rdImport.Text = "Xuất";
-            this.rdImport.UseVisualStyleBackColor = true;
             // 
             // rdExport
             // 
             this.rdExport.AutoSize = true;
-            this.rdExport.Location = new System.Drawing.Point(137, 7);
+            this.rdExport.Location = new System.Drawing.Point(137, 6);
             this.rdExport.Name = "rdExport";
-            this.rdExport.Size = new System.Drawing.Size(50, 18);
+            this.rdExport.Size = new System.Drawing.Size(51, 17);
             this.rdExport.TabIndex = 2;
             this.rdExport.TabStop = true;
             this.rdExport.Text = "Nhập";
             this.rdExport.UseVisualStyleBackColor = true;
             // 
+            // rdImport
+            // 
+            this.rdImport.AutoSize = true;
+            this.rdImport.Location = new System.Drawing.Point(84, 6);
+            this.rdImport.Name = "rdImport";
+            this.rdImport.Size = new System.Drawing.Size(47, 17);
+            this.rdImport.TabIndex = 1;
+            this.rdImport.TabStop = true;
+            this.rdImport.Text = "Xuất";
+            this.rdImport.UseVisualStyleBackColor = true;
+            // 
             // ImExManage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 299);
+            this.ClientSize = new System.Drawing.Size(664, 278);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnView);
             this.Controls.Add(this.btnEdit);
