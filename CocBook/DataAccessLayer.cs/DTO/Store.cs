12f73a40242f5969c97d85a6e5706dfb74868175
GiangHHTSE60894 <GiangHHTSE60894@fpt.edu.vn>
2013-06-20 16:56:02 +0000

    Add Store.cs

---
+++ CocBook/DataAccessLayer.cs/DTO/Store.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccessLayer.cs.DTO
+{
+    public class Store
+    {
+        public string ISBNBook { get; set; }
+        public string BookName { get; set; }
+        public string Publisher { get; set; }
+        public string Unit { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
